6299448de15d38bf329e0bce99bbfc6158d360c0
Ruslan Tukhfyatullov <ruslan.tukhfyatullov@gmail.com>
2025-09-26 00:18:09 +0300

    Fix Vercel routing for React Router

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react-swc';
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     sourcemap: true,
+//   },
+// });
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     sourcemap: true,
   },
